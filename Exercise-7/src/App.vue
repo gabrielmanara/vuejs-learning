59ce2ac63d73a70e4cdcec4127b00540d4f45dcd
CIT\gmanara <gmanara@ciandt.com>
2017-02-14 22:46:28 -0200

    Exercise 7 about components, need some structure folders changes

---
+++ Exercise-7/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <app-header></app-header>
+        <hr>  
+        <div class="row">
+            <app-server-status></app-server-status>
+            <app-server></app-server>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+
+
+<script>
+  import ServerStatus from './ServerStatus.vue';
+  import Server from './Server.vue';
+  import Header from './Header.vue';
+  import Footer from './Footer.vue';
+  
+  export default {
+    components: {
+      'app-server-status': ServerStatus,
+      'app-header': Header,
+      'app-footer': Footer,
+      'app-server': Server
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ Exercise-7/src/ServerStatus.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+      <ul class="list-group">
+          <li v-for="index in 5" class="list-group-item">
+                Server #{{ index }}
+          </li>
+      </ul>
+  </div>
+</template>
