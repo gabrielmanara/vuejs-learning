98e2e4561c00125847912861401ef91dc001e79c
CIT\gmanara <gmanara@ciandt.com>
2017-03-08 21:51:22 -0300

    Module 8 about communication between components using events bus, emit etc

--- Exercise-7/src/App.vue
+++ Exercise-7/src/App.vue
@@ -1,34 +1,26 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>  
         <div class="row">
-            <app-server-status></app-server-status>
-            <app-server></app-server>
+            <div class="col-xs-12">
+                <app-user></app-user>
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
-
-
 <script>
-  import ServerStatus from './components/Server/ServerStatus.vue';
-  import Server from './components/Server/Server.vue';
-  import Header from './components/Shared/Header.vue';
-  import Footer from './components/Shared/Footer.vue';
-  
-  export default {
-    components: {
-      'app-server-status': ServerStatus,
-      'app-header': Header,
-      'app-footer': Footer,
-      'app-server': Server
+    import User from './components/User.vue';
+
+    export default {
+        components: {
+            appUser: User
+        }
     }
-  }
 </script>
 
 <style>
-
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
 </style>
