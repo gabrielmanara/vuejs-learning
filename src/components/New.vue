1eee447f9f5b60c8c4506e94da70e313df9afee7
CIT\gmanara <gmanara@ciandt.com>
2017-03-27 22:43:35 -0300

    Advanced component usage with slots and dynamic components

---
+++ src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Increase</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        counter: 0
+      };
+    },
+    deactivaded() {
+      console.log('Deactivaded');
+    },
+    destroyed() {
+      console.log('Destroyed');
+    },
+    activated() {
+      console.log('Activate');
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Quote.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
+      <span><slot name="subtitle">The subtitle</slot></span>
+    </div>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 15px auto;
+    text-align: center;
+  }
+
+  title {
+    font-family: italic;
+  }
+
+</style>
